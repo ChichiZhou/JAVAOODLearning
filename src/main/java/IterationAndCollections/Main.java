5c26bd1b67fd746b99fe6f24301699b92b05f500
He Zhou <hezhou@HedeMacBook-Pro.lan>
2020-02-09 21:29:22 -0600

    Add collection.iterator

--- src/main/java/IterationAndCollections/Main.java
+++ src/main/java/IterationAndCollections/Main.java
@@ -6,6 +6,10 @@ public class Main {
          * Iterator 的用法就是用在 while 循环中
          * iterator.hasnext() 用来进行循环的判断
          * iterator.next() 用来返回下一个元素
+         *
+         * 凡是实现 collection 接口的，都有一个 iterator
+         * 可以用 collection.iterator() 来返回这个 iterator
+         *
          */
         MenuItem menuItem = new MenuItem("Ass", "Shilin's ass", 1.2);
     }
