ab17bd6d5a9231d0e8f4d7af8dbc3d1b6cf80687
He Zhou <hezhou@HedeMacBook-Pro.lan>
2020-08-07 15:29:46 -0500

    Add comments

--- src/main/java/GoF/adapter/CaramelAdapter.java
+++ src/main/java/GoF/adapter/CaramelAdapter.java
@@ -2,11 +2,16 @@ package GoF.adapter;
 
 import GoF.adapter.avaFilters.Caramel;
 
+/**
+ * 使用继承。目的是用来将 Caramel 包装成 Filter 这个类
+ */
 public class CaramelAdapter extends Caramel implements Filter{
+
+
     @Override
     public void apply(Image image) {
+        // 效果是一样的
         init();
         render(image);
-
     }
 }
--- src/main/java/GoF/adapter/CaramelFilter.java
+++ src/main/java/GoF/adapter/CaramelFilter.java
@@ -2,6 +2,10 @@ package GoF.adapter;
 
 import GoF.adapter.avaFilters.Caramel;
 
+/**
+ * 使用 compostion 目的是用来将 Caramel 包装成 Filter 这个类
+ */
+
 public class CaramelFilter implements Filter{
     private Caramel caramel;
 
